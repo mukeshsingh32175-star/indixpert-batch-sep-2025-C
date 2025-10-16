4648c8418b73e0089b7d2c87ede411ee47e7119a
mukeshsingh32175-star <mukeshsingh32175@gmail.com>
2025-10-16 17:51:02 +0530

    new function

---
+++ trask.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int vew ()
+{
+  int view
+  {
+    scanf("%d",number[i]);
+
+  }
+
+}
+int exit()
+{
+  int exit;
+  printf("thanks for the time ");
+ 
+
+}
+
+int main()
+{
+    int delete;
+    int view;
+    int exit;       
+    int  number[5];
+   int option;
+
+   printf("enter any five number ");
+   for(int i=0;i<=4;i++)
+    {
+      scanf("%d",&number[i]);
+    }
+  
+ 
+   printf("\nmenu\n");
+
+   printf("\n1.delete\n");
+   printf("\n2.view\n");
+   printf("\n3.exit\n");
+
+
+
+    printf("enter any option ");
+   {
+     scanf("%d",option );
+   }
+
+
+
+  return 0;
+}
